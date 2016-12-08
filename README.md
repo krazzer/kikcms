3311b509e2efbefe593029ce045182cec4f76908
Kaz van Wel <kaz@iMac-van-Kaz.local>
2016-12-08 09:32:20 +0100

    KikCMS git POC

--- README.md
+++
@@ -1,4 +0,0 @@
-kikcms
-======
-
-A Symfony project created on March 9, 2016, 7:59 pm.
---
+++ src/KikCMS/cmspoc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KikCMS;
+
+class CmsPoc
+{
+    public function test()
+    {
+        echo 'If you read this your site succesfully uses the KikCMS CmsPoc test!';
+    }
+}
