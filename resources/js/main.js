e942282f76fc080ba0ef28921f856ac31f5a980e
Kaz van Wel <kaz@iMac-van-Kaz.local>
2017-05-12 14:59:44 +0200

    Added role field to datatable (2)

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.action.link').click(function () {
-        var emailAddress        = $(this).prev().prev().text().trim();
+        var emailAddress        = $(this).prev().prev().prev().text().trim();
         var protocolAndHostname = window.location.protocol + '//' + window.location.hostname;
 
         alert(protocolAndHostname + '/cms/login/reset?email=' + emailAddress);
