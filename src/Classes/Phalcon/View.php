b4e330609f7495672d572e8e46c74c3dccbe97da
Kaz van Wel <kaz@iMac-van-Kaz.local>
2017-09-06 15:17:46 +0200

    Throw error when picking a template fails Improved code of View class

--- src/Classes/Phalcon/View.php
+++ src/Classes/Phalcon/View.php
@@ -3,6 +3,7 @@
 namespace KikCMS\Classes\Phalcon;
 
 
+use Exception;
 use Phalcon\Cache\BackendInterface;
 use Phalcon\Mvc\View as PhalconView;
 
@@ -16,14 +17,27 @@ class View extends PhalconView
      */
     protected function _engineRender($engines, $viewPath, $silence, $mustClean, BackendInterface $cache = null)
     {
-        // convert @namespace to full path so that Phalcon accepts it
-        if (strpos($viewPath, '@') === 0) {
-            foreach ($this->namespaces as $namespace => $path) {
-                $viewPath = str_replace('@' . $namespace . '/', $path, $viewPath);
+        $viewPath = $this->convertNamespace($viewPath);
+
+        return parent::_engineRender($engines, $viewPath, $silence, $mustClean, $cache);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function exists($view)
+    {
+        if( ! $this->isNamespaced($view)){
+            return parent::exists($view);
+        }
+
+        foreach ($this->_registeredEngines as $extension => $engine) {
+            if(file_exists($this->convertNamespace($view)) . $extension){
+                return true;
             }
         }
 
-        return parent::_engineRender($engines, $viewPath, $silence, $mustClean, $cache);
+        return false;
     }
 
     /**
@@ -40,5 +54,39 @@ class View extends PhalconView
     public function setNamespaces(array $namespaces)
     {
         $this->namespaces = $namespaces;
+    }
+
+    /**
+     * @param mixed $viewPath
+     * @return PhalconView
+     * @throws Exception
+     */
+    public function pick($viewPath)
+    {
+        if( ! $this->exists($viewPath)){
+            throw new Exception('View "' . $viewPath . '" not found.');
+        }
+
+        return parent::pick($viewPath);
+    }
+
+    /**
+     * @param mixed $viewPath
+     * @return mixed
+     */
+    private function convertNamespace($viewPath)
+    {
+        if ($this->isNamespaced($viewPath)) {
+            foreach ($this->namespaces as $namespace => $path) {
+                $viewPath = str_replace('@' . $namespace . '/', $path, $viewPath);
+            }
+        }
+
+        return $viewPath;
+    }
+
+    private function isNamespaced($viewPath): bool
+    {
+        return strpos($viewPath, '@') === 0;
     }
 }
