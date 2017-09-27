e02d322049d16d35553a41b8cd6f387702d22d46
Kaz van Wel <kaz@iMac-van-Kaz.local>
2017-09-27 17:40:41 +0200

    Add option to format price with 0 decimals

--- src/Services/Util/NumberService.php
+++ src/Services/Util/NumberService.php
@@ -17,16 +17,17 @@ class NumberService extends Injectable
      * Convert a float to a formatted price string
      *
      * @param float $amount
+     * @param int $decimals
      * @return string
      */
-    public function getPriceFormat(float $amount): string
+    public function getPriceFormat(float $amount, int $decimals = 2): string
     {
         $decimalNotation = $this->translator->tl('system.decimalNotation');
 
         if($decimalNotation == 'point'){
-            return number_format($amount, 2, '.', ',');
+            return number_format($amount, $decimals, '.', ',');
         } else {
-            return number_format($amount, 2, ',', '.');
+            return number_format($amount, $decimals, ',', '.');
         }
     }
 }
