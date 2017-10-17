d274a8f201e11715865ca4eb9327b022df45cbfc
Kaz van Wel <kaz@iMac-van-Kaz.local>
2017-10-17 12:02:21 +0200

    +en translation

--- resources/translations/en.php
+++ resources/translations/en.php
@@ -14,6 +14,7 @@ return [
         'noData'            => 'No data found to display...',
         'add'               => 'Add :itemSingular',
         'edit'              => 'Edit :itemSingular',
+        'view'              => 'View :itemSingular',
         'searchPlaceholder' => 'Search',
         'save'              => 'Save',
         'saveAndClose'      => 'Save & close',
