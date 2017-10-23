c204300cbcf0ce5a6996a172288e147bdf1e1cc3
Kaz van Wel <kaz@iMac-van-Kaz.local>
2017-10-23 17:04:24 +0200

    Added uniqueness validator

---
+++ src/Classes/Phalcon/Validator/NewUniqueness.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace KikCMS\Classes\Phalcon\Validator;
+
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Uniqueness;
+
+/**
+ * Extends Uniqueness validator
+ * If the provided id causing the uniqueness error, we can ignore it
+ */
+class NewUniqueness extends Uniqueness
+{
+    /**
+     * @inheritdoc
+     */
+    public function validate(Validation $validation, $field): bool
+    {
+        if ($this->isUniqueness($validation, $field)) {
+            return true;
+        }
+
+        $id     = $this->getOption('id');
+        $model  = $this->getOption('model');
+
+        if( ! $object = $model::getById($id)){
+            return parent::validate($validation, $field);
+        }
+
+        if($object->$field == $validation->getValue($field)){
+            return true;
+        }
+
+        return parent::validate($validation, $field);
+    }
+}
