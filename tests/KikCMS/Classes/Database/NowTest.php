b9299439c0451811ad742681f6f7dbe256a6411a
Kaz van Wel <kaz@iMac-van-Kaz.local>
2018-03-23 13:53:45 +0100

    Added first test

---
+++ tests/KikCMS/Classes/Database/NowTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace KikCMS\Classes\Database;
+
+use PHPUnit\Framework\TestCase;
+
+class NowTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $now = new Now();
+
+        $this->assertEquals(get_class($now), Now::class);
+    }
+
+    public function testToString()
+    {
+        $now = new Now();
+
+        $this->assertEquals(strlen($now->__toString()), 21);
+    }
+}
