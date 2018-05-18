4afc056d451e71d3c8888fc7e09f0da2b5accbb7
Kaz van Wel <>
2018-05-18 09:29:46 +0200

    Added Phonenumber validator

---
+++ src/Classes/Phalcon/Validator/PhoneNumber.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace KikCMS\Classes\Phalcon\Validator;
+
+
+use Phalcon\Validation\Validator\Regex;
+
+class PhoneNumber extends Regex
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct(array $options = null)
+    {
+        $options['pattern'] = '/^[0-9\-\(\)\/\+\s]*$/';
+
+        parent::__construct($options);
+    }
+}
