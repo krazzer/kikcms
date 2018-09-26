2429b04c95352866cc8d971e9e5993c0e4abe551
Kaz van Wel <info@kiksaus.nl>
2018-09-26 14:59:47 +0200

    Convert to RelationKeys

--- src/Util/StringUtil.php
+++ src/Util/StringUtil.php
@@ -10,9 +10,29 @@ class StringUtil
      * @param bool $capitalizeFirstCharacter
      * @return mixed|string
      */
-    public static function dashesToCamelCase($string, $capitalizeFirstCharacter = false)
+    public static function dashesToCamelCase(string $string, bool $capitalizeFirstCharacter = false): string
     {
-        $str = str_replace('-', '', ucwords($string, '-'));
+        return self::toCamelCase($string, '-', $capitalizeFirstCharacter);
+    }
+    /**
+     * @param $string
+     * @param bool $capitalizeFirstCharacter
+     * @return mixed|string
+     */
+    public static function underscoresToCamelCase(string $string, bool $capitalizeFirstCharacter = false): string
+    {
+        return self::toCamelCase($string, '_', $capitalizeFirstCharacter);
+    }
+
+    /**
+     * @param string $string
+     * @param string $charToConvert
+     * @param bool $capitalizeFirstCharacter
+     * @return string
+     */
+    private static function toCamelCase(string $string, string $charToConvert, bool $capitalizeFirstCharacter = false): string
+    {
+        $str = str_replace($charToConvert, '', ucwords($string, $charToConvert));
 
         if (!$capitalizeFirstCharacter) {
             $str = lcfirst($str);
