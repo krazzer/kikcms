5e074eda2121c0d57eb1995d32676308ee081149
Kaz van Wel <info@kiksaus.nl>
2019-03-05 15:23:50 +0100

    Shorter array

--- src/Classes/Phalcon/Twig.php
+++ src/Classes/Phalcon/Twig.php
@@ -92,8 +92,8 @@ class Twig extends Engine implements EngineInterface
         $options = ['is_safe' => ['html']];
 
         $functions = [
-            'allowed', 'config', 'endForm', 'file', 'fileBg', 'form', 'javascriptInclude', 'route', 'stylesheetLink', 'submitButton',
-            'svg', 'tl', 'ucfirst', 'url'
+            'allowed', 'config', 'endForm', 'file', 'fileBg', 'form', 'javascriptInclude', 'route', 'stylesheetLink',
+            'submitButton', 'svg', 'tl', 'ucfirst', 'url'
         ];
 
         /** @var TwigService $twigService */
