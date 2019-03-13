b5393443ffa87ca9b4690a018a316c74c9cf4b41
Kaz van Wel <info@kiksaus.nl>
2019-03-13 16:02:17 +0100

    MCE content replacor

--- src/Tasks/MainTask.php
+++ src/Tasks/MainTask.php
@@ -1,5 +1,6 @@
 <?php
 
+use KikCMS\Models\File;
 use KikCMS\Services\DataTable\PageRearrangeService;
 use KikCMS\Services\Finder\FileHashService;
 use KikCMS\Services\LanguageService;
@@ -9,6 +10,8 @@ use Phalcon\Cli\Task;
  * @property FileHashService $fileHashService
  * @property LanguageService $languageService
  * @property PageRearrangeService $pageRearrangeService
+ *
+ * @property \KikCmsCore\Services\DbService $dbService
  */
 class MainTask extends Task
 {
@@ -25,5 +28,38 @@ class MainTask extends Task
     public function updateMissingFileHashesAction()
     {
         $this->fileHashService->updateMissingHashes();
+    }
+
+    public function symlinkAction()
+    {
+        $query = (new \Phalcon\Mvc\Model\Query\Builder)
+            ->columns(['page_id', 'value'])
+            ->from(\KikCMS\Models\PageLanguageContent::class)
+            ->where('value LIKE :like:', ['like' => '%/finder/file/%']);
+
+        $pageContentList = $this->dbService->getObjects($query);
+
+        foreach ($pageContentList as $pageContent){
+            preg_match_all("/finder\/file\/([0-9]+)/", $pageContent->value, $matches);
+
+            foreach($matches[1] as $fileId){
+                $file = File::getById($fileId);
+
+                $source = SITE_PATH . 'storage/media/' . $file->getFileName();
+                $target = SITE_PATH . 'public_html/media/files/' . $file->getFileName();
+
+                if( ! file_exists($target)){
+                    symlink($source, $target);
+                }
+
+                echo 'Created symlink for: ' . $target . PHP_EOL;
+
+                $this->dbService->query("
+                    UPDATE cms_page_language_content 
+                    SET value = REPLACE(value, '/finder/file/" . $fileId . "\"', '/media/files/" . $file->getFileName() . "\"')");
+            }
+        }
+
+//
     }
 }
