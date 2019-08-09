0a95aab66a463f4c44e66967537fd2d9b829bd24
Kaz van Wel <info@kiksaus.nl>
2019-08-09 14:47:42 +0200

    Fix autoloader for tests

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+spl_autoload_register(function ($class) {
+    if (substr($class, 0, 7) == 'Website') {
+        $class = str_replace('Website\\', '', $class);
+        $class = str_replace('\\', '/', $class);
+
+        include(__DIR__ . '/../TestSitePath/app/' . $class . '.php');
+    }
+});
+
 include_once __DIR__ . '/../TestSitePath/autoloader.php';
 include_once __DIR__ . '/../../vendor/autoload.php';
 include_once __DIR__ . '/../../vendor/kiksaus/kikcms-core/src/functions.php';
