bd422b2e4a723f1d095f079ddab118352e82a497
Kaz van Wel <info@kiksaus.nl>
2019-09-26 16:46:25 +0200

    Add test for pageAction (refactored to FrontendService) wip

---
+++ tests/unit/KikCMS/Services/Website/FrontendServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KikCMS\Services\Website;
+
+
+use PHPUnit_Framework_TestCase;
+
+class FrontendServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetPageLanguageToLoadByUrlPath()
+    {
+        $frontendService = new FrontendService();
+    }
+}
