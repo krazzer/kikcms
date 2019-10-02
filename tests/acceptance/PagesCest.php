15db445ddb4eefbed5849b9da63e226191f76aeb
Kaz van Wel <info@kiksaus.nl>
2019-10-02 08:11:24 +0200

    WIP: Drag and drop test

--- tests/acceptance/PagesCest.php
+++ tests/acceptance/PagesCest.php
@@ -5,6 +5,9 @@ namespace acceptance;
 
 
 use AcceptanceTester;
+use Facebook\WebDriver\Remote\DriverCommand;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
 
 class PagesCest
 {
@@ -100,5 +103,35 @@ class PagesCest
         $I->click('.table tr:nth-child(1) .arrow');
 
         $I->canSeeNumberOfElements('.table tr', 5);
+    }
+
+    public function dragAndDropWorks(AcceptanceTester $I)
+    {
+        $I->addUser();
+        $I->login();
+        $I->amOnPage('/cms/pages');
+
+        $I->executeInSelenium(function (RemoteWebDriver $webDriver) {
+            $element = $webDriver->findElement(WebDriverBy::cssSelector('tr[data-id="6"] span.name'));
+//            $webDriver->execute(DriverCommand::MOVE_TO, [$element->getCoordinates(), 0, 0]);
+
+//            $webDriver->action()->moveToElement($element, 10, 10);
+//            $webDriver->execute(DriverCommand::MOUSE_DOWN);
+
+            $webDriver->action()->clickAndHold($element);
+            $webDriver->action()->moveByOffset(0, -100);
+            $webDriver->execute(DriverCommand::MOUSE_UP);
+
+        });
+
+        $I->wait(1);
+
+        $I->makeScreenshot(1);
+
+//        $I->dragAndDrop('tr[data-id="6"] span.name', 'tr[data-id="3"]');
+//        $I->makeScreenshot(1);
+//
+//        $I->moveMouseOver();
+//        $I->performOn();
     }
 }
