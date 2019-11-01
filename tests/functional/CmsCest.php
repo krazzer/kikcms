8c4c8b4a25edb749acad118cbd0f46b75e49fa82
Kaz van Wel <info@kiksaus.nl>
2019-11-01 15:34:45 +0100

    Added functional tests for CmsController

---
+++ tests/functional/CmsCest.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace functional;
+
+use FunctionalTester;
+
+class CmsCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->login();
+    }
+
+    public function filePickerWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/filepicker');
+        $I->seeElement('.files-container');
+    }
+
+    public function mediaWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/media');
+        $I->seeElement('.files-container');
+    }
+
+    public function settingsWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/settings');
+        $I->seeElement('#webFormId_KikCMSFormsSettingsForm');
+    }
+
+    public function usersWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/users');
+        $I->seeElement('.datatable');
+    }
+
+    public function statsWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/stats');
+        $I->seeElement('#visitors');
+    }
+
+    public function previewWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/preview/3');
+        $I->see('Lorem ipsum dolor sit amet');
+    }
+
+    public function getTinyMceLinksWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/getTinyMceLinks');
+        $I->see('[{"id":"6","parent_id":null');
+    }
+
+    public function getTranslationsForKeyWorks(FunctionalTester $I)
+    {
+        $I->sendAjaxPostRequest('/cms/getTranslationsForKey', ['key' => 'cms.roles']);
+        $I->see('{"nl":"cms.roles"');
+    }
+
+    public function getUrlsWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/get-urls/nl');
+        $I->see('["\/home","\/page-not-found","\/pagina-2"]');
+    }
+
+    public function generateSecurityTokenWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/cms/generate-security-token');
+        $I->see('securityToken');
+    }
+}
