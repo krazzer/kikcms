581d2f49374fc144871a3641e03dd1df0105ab5c
Kaz van Wel <info@kiksaus.nl>
2019-11-11 15:04:59 +0100

    Correctly set WebForm input as 1 or 0 instead of boolean

--- src/Classes/WebForm/WebForm.php
+++ src/Classes/WebForm/WebForm.php
@@ -218,7 +218,7 @@ abstract class WebForm extends Renderable
         foreach ($this->getFieldMap() as $key => $field){
             // in case of a checkbox, set the value by its existence
             if ($field->getType() == Field::TYPE_CHECKBOX) {
-                $input[$key] = isset($input[$key]);
+                $input[$key] = isset($input[$key]) ? 1 : 0;
             }
 
             // in case of a multicheckbox, and nothing is present, set value as an empty array
