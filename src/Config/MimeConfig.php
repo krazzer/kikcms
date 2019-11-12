22da8f51071e18d2e2cfcfc303fc6d2af080c659
Kaz van Wel <info@kiksaus.nl>
2019-11-12 08:27:20 +0100

    Add application/octet-stream as allowed docx mime

--- src/Config/MimeConfig.php
+++ src/Config/MimeConfig.php
@@ -26,7 +26,7 @@ class MimeConfig
         'gif'  => ['image/gif'],
         'pdf'  => ['application/pdf', 'image/pjpeg'],
         'doc'  => ['application/msword'],
-        'docx' => ['application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+        'docx' => ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/octet-stream'],
         'xls'  => ['application/vnd.ms-excel'],
         'xlsx' => ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
         'ppt'  => ['application/vnd.ms-powerpoint'],
