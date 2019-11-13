8b232520d3b5b71ca21033115be713beda39cac6
Kaz van Wel <info@kiksaus.nl>
2019-11-13 16:56:06 +0100

    Added tests for RobotsController

---
+++ tests/functional/RobotsCest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace functional;
+
+use FunctionalTester;
+
+class RobotsCest
+{
+    public function sitemapWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/sitemap.xml');
+        $I->see('https://kikcmstest.dev/home');
+    }
+
+    public function robotsWorks(FunctionalTester $I)
+    {
+        $I->amOnPage('/robots.txt');
+        $I->see('User-agent: *');
+    }
+}
