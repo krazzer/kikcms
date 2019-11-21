e64fb3510b2d6923122fe0d9ecf78899f08a16cf
Kaz van Wel <info@kiksaus.nl>
2019-11-21 11:05:46 +0100

    Added functional GenerateTask tests

--- tests/functional/GenerateTaskCest.php
+++ tests/functional/GenerateTaskCest.php
@@ -4,14 +4,59 @@ declare(strict_types=1);
 namespace functional;
 
 use FunctionalTester;
+use Phalcon\Db\Column;
 
 class GenerateTaskCest
 {
-    public function managerWorks(FunctionalTester $I)
+    const FILES = [
+        __DIR__ . '/../TestSitePath/app/Models/GenerateTest.php',
+        __DIR__ . '/../TestSitePath/app/Forms/GenerateTestForm.php',
+        __DIR__ . '/../TestSitePath/app/ObjectList/GenerateTestList.php',
+        __DIR__ . '/../TestSitePath/app/ObjectList/GenerateTestMap.php',
+        __DIR__ . '/../TestSitePath/app/DataTables/GenerateTests.php',
+        __DIR__ . '/../TestSitePath/app/Services/GenerateTestService.php',
+    ];
+
+    public function modelsWorks(FunctionalTester $I)
     {
-        //wip
-//        passthru('ls');
-//        $I->get
-//        $I->runShellCommand('php tests/TestSitePath/kikcms generate models');
+        $I->getDbService()->db->dropTable('test_generate_test');
+        $I->getDbService()->db->createTable('test_generate_test', null, [
+            'columns' => [new Column('id', ['type' => Column::TYPE_INTEGER, 'size' => 11])],
+        ]);
+
+        $I->runShellCommand('php tests/TestSitePath/kikcms generate models');
+
+        $I->getDbService()->db->dropTable('test_generate_test');
+
+        foreach (self::FILES as $file){
+            $I->assertTrue(file_exists($file));
+        }
+
+        $this->_deleteFiles();
+    }
+
+    public function modelWorks(FunctionalTester $I)
+    {
+        $I->getDbService()->db->dropTable('test_generate_test');
+        $I->getDbService()->db->createTable('test_generate_test', null, [
+            'columns' => [new Column('id', ['type' => Column::TYPE_INTEGER, 'size' => 11])],
+        ]);
+
+        $I->runShellCommand('php tests/TestSitePath/kikcms generate model test_generate_test');
+
+        $I->getDbService()->db->dropTable('test_generate_test');
+
+        foreach (self::FILES as $file){
+            $I->assertTrue(file_exists($file));
+        }
+
+        $this->_deleteFiles();
+    }
+
+    private function _deleteFiles()
+    {
+        foreach (self::FILES as $file){
+            unlink($file);
+        }
     }
 }
