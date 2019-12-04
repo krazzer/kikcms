4bb82620399f79f1c7b2aab6a0ec452aac5c7ad0
Kaz van Wel <info@kiksaus.nl>
2019-12-04 11:09:14 +0100

    Added TreeOrder test

---
+++ tests/functional/PagesDataTableCest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace functional;
+
+use FunctionalTester;
+
+class PagesDataTableCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->login();
+    }
+
+    public function treeOrderWorks(FunctionalTester $I)
+    {
+        $I->sendAjaxPostRequest('/cms/datatable/pages/tree-order', [
+            'renderableInstance' => 'dataTable5dc40ab26a399',
+            'renderableClass'    => 'KikCMS\DataTables\Pages',
+            'activeLangCode'     => 'nl',
+            'pageId'             => '3',
+            'targetPageId'       => '4',
+            'position'           => 'after',
+        ]);
+
+        $result = $I->getDbService()->queryValues('SELECT id FROM cms_page WHERE parent_id = 5 ORDER BY display_order');
+
+        $I->assertEquals([4,3], $result);
+
+        $I->sendAjaxPostRequest('/cms/datatable/pages/tree-order', [
+            'renderableInstance' => 'dataTable5dc40ab26a399',
+            'renderableClass'    => 'KikCMS\DataTables\Pages',
+            'activeLangCode'     => 'nl',
+            'pageId'             => '3',
+            'targetPageId'       => '4',
+            'position'           => 'before',
+        ]);
+
+        $result = $I->getDbService()->queryValues('SELECT id FROM cms_page WHERE parent_id = 5 ORDER BY display_order');
+
+        $I->assertEquals([3,4], $result);
+
+        $I->sendAjaxPostRequest('/cms/datatable/pages/tree-order', [
+            'renderableInstance' => 'dataTable5dc40ab26a399',
+            'renderableClass'    => 'KikCMS\DataTables\Pages',
+            'activeLangCode'     => 'nl',
+            'pageId'             => '6',
+            'targetPageId'       => '5',
+            'position'           => 'into',
+        ]);
+
+        $result = $I->getDbService()->queryValues('SELECT id FROM cms_page WHERE parent_id = 5 ORDER BY display_order');
+
+        $I->assertEquals([3,4,6], $result);
+    }
+}
