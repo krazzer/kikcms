55fef467a565580d3fb447fbef115ed3b45f5898
Kaz van Wel <info@kiksaus.nl>
2019-12-19 13:34:17 +0100

    Added unit-functional tests for DateTimeService

---
+++ tests/unit/Services/Util/DateTimeServiceTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Services\Util;
+
+use Helpers\Unit;
+use KikCMS\Services\Util\DateTimeService;
+use Phalcon\Validation\Validator\Date;
+
+class DateTimeServiceTest extends Unit
+{
+    public function testStringToDateFormat()
+    {
+        $dateTimeService = new DateTimeService();
+        $dateTimeService->setDI($this->getDbDi());
+
+        $this->assertEquals('', $dateTimeService->stringToDateFormat(''));
+        $this->assertEquals('jan 20 2010', $dateTimeService->stringToDateFormat('2010-01-20'));
+        $this->assertEquals('', $dateTimeService->stringToDateTimeFormat(''));
+        $this->assertEquals('jan 20 2010, at 10:10', $dateTimeService->stringToDateTimeFormat('2010-01-20 10:10:10'));
+    }
+
+    public function testGetValidator()
+    {
+        $dateTimeService = new DateTimeService();
+        $dateTimeService->setDI($this->getDbDi());
+
+        $this->assertInstanceOf(Date::class, $dateTimeService->getValidator());
+    }
+
+    public function testGetFromDatePickerValue()
+    {
+        $dateTimeService = new DateTimeService();
+        $dateTimeService->setDI($this->getDbDi());
+
+        $this->assertEquals('20-01-2020', $dateTimeService->getFromDatePickerValue('2020-01-20')->format('d-m-Y'));
+    }
+
+    public function testGetDateFormat()
+    {
+        $dateTimeService = new DateTimeService();
+        $dateTimeService->setDI($this->getDbDi());
+
+        $this->assertEquals('Y-m-d', $dateTimeService->getDateFormat());
+    }
+}
