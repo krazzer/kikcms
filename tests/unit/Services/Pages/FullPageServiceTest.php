78628779b6aa4ec2f1d894086b02478519a66c51
Kaz van Wel <info@kiksaus.nl>
2020-01-10 09:55:35 +0100

    Added test for FullPageService::getByPage

---
+++ tests/unit/Services/Pages/FullPageServiceTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Services\Pages;
+
+use Helpers\Unit;
+use KikCMS\Models\Page;
+use KikCMS\Models\PageContent;
+use KikCMS\Models\PageLanguage;
+use KikCMS\Services\Pages\FullPageService;
+
+class FullPageServiceTest extends Unit
+{
+    public function testGetByPage()
+    {
+        $fullPageService = new FullPageService();
+        $fullPageService->setDI($this->getDbDi());
+
+        $page     = new Page();
+        $page->id = 1;
+
+        // not found
+        $this->assertNull($fullPageService->getByPage($page));
+
+        // found
+        $page->save();
+
+        $pageLanguage                = new PageLanguage();
+        $pageLanguage->page_id       = 1;
+        $pageLanguage->language_code = 'en';
+        $pageLanguage->name          = 'test';
+
+        $pageLanguage->save();
+
+        $fullPage = $fullPageService->getByPage($page);
+
+        $this->assertEquals(1, $fullPage->getId());
+
+        // with content
+        $pageContent                = new PageContent();
+        $pageContent->page_id       = 1;
+        $pageContent->value         = 'val';
+        $pageContent->field         = 'key';
+
+        $pageContent->save();
+
+        $fullPage = $fullPageService->getByPage($page);
+
+        $this->assertEquals('val', $fullPage->get('key'));
+    }
+}
