7eb77328cd7a462453ebc1030a7cfa468633edf1
Kaz van Wel <info@kiksaus.nl>
2020-01-28 10:35:35 +0100

    Try

--- tests/functional/_phalcon_bootstrap.php
+++ tests/functional/_phalcon_bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-$_SERVER['HTTP_HOST'] = 'test.dev';
-
 $sitePath = __DIR__ . '/../TestSitePath/';
 
 /** @var Phalcon\Application $app */
 $app = include __DIR__ . '/../../boot/app.php';
+
+$this->client->setServerParameter('HTTP_HOST', 'kikcmstest.dev');
 
 return $app;
