ce6445a1189d4b83b59664d2ba221a4cd6f6ed01
krazzer <info@kiksaus.nl>
2020-03-27 14:38:45 +0100

    Added tests for PagesFlat

---
+++ tests/unit/DataTables/PagesFlat/PagesFlatTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace DataTables;
+
+use Helpers\DataTables\PagesFlat;
+use Helpers\TestHelper;
+use Helpers\Unit;
+use KikCMS\DataTables\Filters\PagesDataTableFilters;
+use KikCMS\Forms\PageForm;
+use KikCmsCore\Services\DbService;
+
+class PagesFlatTest extends Unit
+{
+    public function testGetDefaultQuery()
+    {
+        $filters = new PagesDataTableFilters();
+        $filters->setLanguageCode('en');
+
+        $pagesFlat = new PagesFlat();
+        $pagesFlat->setFilters($filters);
+
+        $dbService = $this->createMock(DbService::class);
+        $dbService->method('getAliasForModel')->willReturn('alias');
+
+        $pagesFlat->dbService = $dbService;
+
+        $this->assertNotNull($pagesFlat->getDefaultQuery());
+    }
+
+    public function testGetFormClass()
+    {
+        $pages = new PagesFlat();
+
+        $this->assertEquals(PageForm::class, $pages->getFormClass());
+    }
+
+    public function testGetJsData()
+    {
+        $pages = new PagesFlat();
+
+        $pages->translator = (new TestHelper)->getTranslator();
+
+        $this->assertIsArray($pages->getJsData()['properties']);
+    }
+}
