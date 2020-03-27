7de3c159f03a449eb5e62278e7eba4b26429cf4e
krazzer <info@kiksaus.nl>
2020-03-27 15:08:41 +0100

    Added tests for Users

---
+++ tests/unit/DataTables/UsersTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace DataTables;
+
+use Helpers\Unit;
+use KikCMS\DataTables\Users;
+use KikCMS\Forms\UserForm;
+
+class UsersTest extends Unit
+{
+    public function testGetFormClass()
+    {
+        $users = new Users();
+
+        $this->assertEquals(UserForm::class, $users->getFormClass());
+    }
+}
