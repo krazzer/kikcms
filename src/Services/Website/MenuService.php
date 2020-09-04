cf8e01dc42943cd4d0a4adec8a55611d73d031a9
krazzer <info@kiksaus.nl>
2020-09-04 14:23:05 +0200

    Fix menu caching for multiple templates

--- src/Services/Website/MenuService.php
+++ src/Services/Website/MenuService.php
@@ -39,7 +39,7 @@ class MenuService extends Injectable
             $menu->getLanguageCode(),
             $menu->getMaxLevel(),
             $menu->getTemplate() ?: 'default',
-            $menu->getRestrictTemplates() ?: 'null'
+            $menu->getRestrictTemplates() ? implode('-', $menu->getRestrictTemplates()) : 'null'
         );
     }
 }
