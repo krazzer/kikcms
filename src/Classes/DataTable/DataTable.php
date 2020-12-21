08243003cc76c5230caf3adf2cc504e300d0a425
Kaz van Wel <info@kiksaus.nl>
2020-12-21 10:59:08 +0100

    Add alias to sortable field

--- src/Classes/DataTable/DataTable.php
+++ src/Classes/DataTable/DataTable.php
@@ -595,6 +595,10 @@ abstract class DataTable extends Renderable
         $customFilters   = $this->getCustomFilters();
         $searchFields    = $this->getSearchableFields();
 
+        if ($alias = $this->getAlias()) {
+            $sortableField = $alias . '.' . $sortableField;
+        }
+
         $this->dataTableFilterService->addSearchFilter($query, $filters, $searchFields);
         $this->dataTableFilterService->addSortFilter($query, $filters, $isSortable, $sortableField);
         $this->dataTableFilterService->addSubDataTableFilter($query, $filters, $cachedNewIds, $aliasedTableKey);
