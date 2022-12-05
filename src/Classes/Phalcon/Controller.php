9d76b7ddd6893c820f0b70e4c1bd7851752992a9
Kaz van Wel <info@kiksaus.nl>
2022-12-05 11:55:41 +0100

    Added convenience function for displaying a frontend template

--- src/Classes/Phalcon/Controller.php
+++ src/Classes/Phalcon/Controller.php
@@ -6,7 +6,11 @@ namespace KikCMS\Classes\Phalcon;
 
 use Phalcon\Http\ResponseInterface;
 use Phalcon\Mvc\ControllerInterface;
+use Website\Classes\TemplateVariables;
 
+/**
+ * @property TemplateVariables $templateVariables
+ */
 abstract class Controller extends Injectable implements ControllerInterface
 {
     /**
@@ -27,5 +31,20 @@ abstract class Controller extends Injectable implements ControllerInterface
         }
 
         return $this->response->setContent($this->view->getPartial($view, $parameters));
+    }
+
+    /**
+     * @param string $view
+     * @param array $parameters
+     * @param int|null $statusCode
+     * @return ResponseInterface
+     */
+    protected function frontendView(string $view, array $parameters, int $statusCode = null): ResponseInterface
+    {
+        $globalParameters = $this->templateVariables->getGlobalVariables();
+
+        $globalParameters['helper'] = $this->frontendHelper;
+
+        return $this->view($view, $globalParameters + $parameters, $statusCode);
     }
 }
